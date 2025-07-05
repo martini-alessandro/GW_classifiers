71bcb10c71aeaffd6ebad3e46dc8bbc79ab42187
martini-alessandro <martini.alessandr@gmail.com>
2025-07-05 17:06:44 +0200

    Added metrics for import

--- GW_classifiers/__init__.py
+++ GW_classifiers/__init__.py
@@ -2,4 +2,6 @@ from .device import moveTo
 from .training import train_network 
 from .neuralnetwork import FFNetwork 
 from .plot import * 
+from .dataprocessing import preprocessData 
+from .metrics import auc_metric 
 
