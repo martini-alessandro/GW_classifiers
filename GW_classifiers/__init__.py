c6f7dbf926134d116dd01619aa54135604b7cb8a
martini-alessandro <martini.alessandr@gmail.com>
2025-08-02 17:42:08 +0200

    Renamed dir

--- GW_classifiers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .training import train_network 
-from .models import FFNetwork 
-from .plot import * 
-from .dataprocessing import loadAndPreprocess
-from .metrics import auc_metric 
-from .utils import *  
