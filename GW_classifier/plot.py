047c0f5030da04d8641b55569f7697d0013aa771
martini-alessandro <martini.alessandr@gmail.com>
2025-08-03 11:07:29 +0200

    Added ROC curve and Precision-Recall plot

--- GW_classifier/plot.py
+++ GW_classifier/plot.py
@@ -12,11 +12,26 @@ def plot_cv():
 def plot_confusion_matrix(): 
     return 
 
-def plot_roc_curve():
-    return 
+def plot_roc_curve(fpr, tpr, roc_auc):  
+    fig, ax = plt.subplots() 
+    ax.plot(fpr, tpr, label=f'ROC curve (area = {roc_auc:.2f})')
+    ax.plot([0, 1], [0, 1], 'k--', label='Random guess')
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Receiver Operating Characteristic (ROC) Curve')
+    ax.legend(loc='lower right')
+    plt.show()
+    return fig, ax 
 
-def plot_precision_recall_curve():
-    return 
+def plot_precision_recall_curve(recall, precision, pr_auc):
+    fig, ax = plt.subplots()
+    ax.plot(recall, precision, label=f'Precision-Recall curve (area = {pr_auc:.2f})')
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.set_title('Precision-Recall Curve')
+    ax.legend(loc='lower left')
+    plt.show()
+    return fig, ax 
 
 def plot_feature_importance(): 
     return 
