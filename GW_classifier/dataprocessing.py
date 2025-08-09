ed7738b09840e68da137847e1c4d80e14f0c7545
martini-alessandro <martini.alessandr@gmail.com>
2025-08-09 15:35:20 +0200

    Added logger import and INFO

--- GW_classifier/dataprocessing.py
+++ GW_classifier/dataprocessing.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np 
+import os
+from GW_classifier.metrics import auc_metric 
+from GW_classifier.plot import * 
+from GW_classifier.logger import get_logger
+
+
+logger = get_logger(__name__)
 
 columns_to_drop =  [
                     'rho1','duration0','bandwidth0','frequency0','time1','chirp1','chirp3','chirp5',\
@@ -17,6 +24,7 @@ def load_and_preprocess(data_dir):
     Returns:
     pd.DataFrame: The preprocessed DataFrame with NaN values removed and index reset.
     """
+    logger.info(f"Loading data from {data_dir}")
     df = pd.read_csv(data_dir)
     df = df.dropna()
     df.reset_index(drop=True, inplace=True)
@@ -32,3 +40,21 @@ def load_and_preprocess(data_dir):
     df = df.drop(columns = ['noise0', 'noise1'])
 
     return df.drop(columns = 'class'), df['class']
+
+def postprocess_and_plot(config, model, y_test, y_hat): 
+    """ 
+    Postprocess the results and generate plots based on the configuration.
+    """
+    savedir = f"{config['savepath']}"
+    os.makedirs(savedir, exist_ok=True)
+
+    if 'roc' in config['scoring']: 
+        fig, ax = plot_auc(y_test, y_hat, method='roc')
+
+        fig.savefig(os.path.join(savedir, f'ROC.png'), bbox_inches='tight')
+    if 'precision recall' in config['scoring']:
+        fig, ax = plot_auc(y_test, y_hat, method='precision recall')
+        fig.savefig(os.join(savedir, f'Precision_Recall.png'), bbox_inches='tight')
+
+
+
--- GW_classifier/pipeline.py
+++ GW_classifier/pipeline.py
@@ -1,24 +1,32 @@
 from argparse import ArgumentParser
-from GW_classifier.dataprocessing import load_and_preprocess
+from GW_classifier.dataprocessing import load_and_preprocess, postprocess_and_plot
 from GW_classifier.training import train_model 
-from GW_classifier.utils import load_module
+from GW_classifier.utils import load_module, create_directory 
+from GW_classifier.logger import get_logger
 from sklearn.model_selection import train_test_split 
 import numpy as np 
 import json 
 
+
+logger = get_logger(__name__)
+
 def main(config): 
+    logger.info("Starting the training pipeline")
     if 'seed' in config:
         np.random.seed(config['seed'])
+    #Load and scale the data 
     X, y = load_and_preprocess(config['datasource'])
     scaler = load_module("sklearn.preprocessing", config['scaler'])
     X = scaler.fit_transform(X) 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.05, random_state=config['seed'])
-    model = train_model(config, X_train, y_train) 
-    if hasattr(model, "predict"): 
-        y_hat = model.predict(X_test)
-    else:
-        y_hat = model(X_test) 
 
+    #Split in train and test 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.05, random_state=config['seed'])
+    model, results = train_model(config, X_train, y_train) 
+    
+    #Predict labes for test set 
+    y_hat = model.predict(X_test) if hasattr(model, "predict") else model(X_test) 
+
+    postprocess_and_plot(config, model, y_test, y_hat)
     return 0 
 
 
