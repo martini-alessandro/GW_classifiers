7eb7f99ca7830f82820fa305d8b8ebb1289c0263
martini-alessandro <martini.alessandr@gmail.com>
2025-08-10 17:05:26 +0200

    now contains load model and save model

--- GW_classifier/utils.py
+++ GW_classifier/utils.py
@@ -1,5 +1,29 @@
 from importlib import import_module
+import joblib 
+import os 
 
+
+def save_model(model, name, path = "results/models"):
+    """ 
+    Save a model to a file using joblib.
+    Parameters:
+    model (nn.Module): The PyTorch model to save.
+    name (str): The name of the model file.
+    path (str): The directory where the model will be saved.
+    """ 
+    joblib.dump(model, os.path.join(path, f"{name}.pkl"))
+
+
+def load_model(name, path = "results/models"): 
+    """
+    Loads a model using joblib 
+    Parameters: 
+    -name: The name of the file 
+    -path: The path where the models is stored 
+    Return: 
+    -The Loaded model 
+    """
+    return joblib.load(os.path.join(path, f"{name}.pkl"))
 
 def load_module(path, name): 
     """
@@ -13,6 +37,28 @@ def load_module(path, name):
     module_path = import_module(path) 
     module_name = getattr(module_path, name)
     return module_name() 
+
+def create_directory(config): 
+    """
+    Creates a directory for saving results based on the configuration.
+    
+    Parameters:
+    config (dict): Configuration dictionary containing the 'savepath'.
+    
+    Returns:
+    str: The path to the created directory.
+    """
+    #Check if 'savepath' is in the config
+    if not config.get('savepath'):
+        raise ValueError("Configuration must contain 'savepath' key.")
+    save_path = config.get('savepath') 
+    #Create the save directory if it does not exist
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+        os.makedirs(os.path.join(save_path, 'pipeline'))
+        os.makedirs(os.path.join(save_path, 'results'))
+        os.makedirs(os.path.join(save_path, 'plots'))
+    return save_path
 
 def moveTo(obj, device): 
     """
